dc86a09f5c81bddfa7b6a99cd896927e871d38bb
SHOHAM212 <shoamhadar@gmail.com>
2023-04-16 18:14:13 +0300

    end of stage 2

--- unitTests/geometries/CylinderTest.java
+++ unitTests/geometries/CylinderTest.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CylinderTest {
-
-    @Test
-    void getHeight() {
-    }
+class CylinderTest
+        @Test
+        void getHeight() {
+        }
 }
